d39abc874ca02ab5c117aff577111764fb97af4a
Bartek Tofel <bartlomiej.tofel@teamcmp.com>
2017-10-27 15:19:22 +0200

    Added a commandline hash generator/checker

---
+++ hasher.php
@@ -0,0 +1,30 @@
+#!/usr/bin/env php
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+if (!isset($argv[1]) || !isset($argv[2])) {
+    echo 'usage: hasher <hash|check> <payload> [<hash>]' . PHP_EOL;
+    exit(1);
+}
+
+$command = strpos($argv[1], 'hash') !== false ? 0 : 1;
+$payload = $argv[2];
+$hash = null;
+
+if ($command == 1 && !isset($argv[3])) {
+    echo 'need both payload and hash to check it'. PHP_EOL;
+    exit(1);
+} elseif ($command == 1 && isset($argv[3])) {
+    $hash = $argv[3];
+}
+
+$secretKey = '{SLfwK{K]3~FND:U';
+
+$hasher = new \Pluggit\Hasher\Hasher($secretKey);
+
+if ($command == 0) {
+    echo $hasher->hash($payload). PHP_EOL;
+} else {
+    echo $hasher->isValid($payload, $hash) ? 'valid'. PHP_EOL : 'invalid'. PHP_EOL;
+}
